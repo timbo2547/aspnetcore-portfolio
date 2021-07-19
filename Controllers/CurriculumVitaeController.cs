86358a7cfd170c0e79e6a2cffc3deb5ffedac45b
timbo2547 <timbo2547@gmail.com>
2021-07-19 02:42:41 -0500

    added cv and new nav setup

---
+++ Controllers/CurriculumVitaeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TimPortfolioApp.Controllers
+{
+    public class CurriculumVitaeController : Controller
+    {
+        //[HttpGet("{id}")]
+        //[Route("Stream")]
+        public IActionResult Index()
+        {
+            string filePath = "~/docs/cv.pdf";
+            Response.Headers.Add("Content-Disposition", "inline; filename=cv.pdf");
+            return File(filePath, "application/pdf");
+        }
+    }
+}
